910b3a64bb7eacb7efc339be93f10b7d76d3b581
Travis CI User <travis@example.org>
2018-06-18 16:40:45 +0000

    Update documentation

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vst"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vst/api/flags/struct.Channel.html\" title=\"struct vst::api::flags::Channel\">Channel</a>",synthetic:false,types:["vst::api::flags::Channel"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vst/api/flags/struct.Plugin.html\" title=\"struct vst::api::flags::Plugin\">Plugin</a>",synthetic:false,types:["vst::api::flags::Plugin"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vst/api/flags/struct.ModifierKey.html\" title=\"struct vst::api::flags::ModifierKey\">ModifierKey</a>",synthetic:false,types:["vst::api::flags::ModifierKey"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vst/api/flags/struct.MidiEvent.html\" title=\"struct vst::api::flags::MidiEvent\">MidiEvent</a>",synthetic:false,types:["vst::api::flags::MidiEvent"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
